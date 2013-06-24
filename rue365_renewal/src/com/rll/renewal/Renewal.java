5c68acd1d2604a4682d5f1ad2ea666385f91e3b0
aliu <aliu@ruelala.com>
2013-06-24 15:45:53 -0400

    Add in basic functionalities in main

--- rue365_renewal/src/com/rll/renewal/Renewal.java
+++ rue365_renewal/src/com/rll/renewal/Renewal.java
@@ -1,8 +1,38 @@
 package com.rll.renewal;
 
 public class Renewal {
+	private static void printUsage() {
+		System.out.println("Renewal program takes in one of the 4 parameters: \n" +
+				"pull	-- Pull and Assign\n" +
+				"email	-- Send promotion emails\n" +
+				"renew	-- auto renewal (place order and add credit)\n" +
+				"all	-- do all of the above"
+		);
+	}
 	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.out.println("Invalid parameters!");
+			printUsage();
+			System.exit(1);
+		}
 		
-		
+		String param = args[0];
+		if (param.equals("pull")) {
+			// call into Pull.java
+			System.out.println("pull was called");
+		} else if (param.equals("email")) {
+			// call into Email.java
+			System.out.println("email was called");
+		} else if (param.equals("renew")) {
+			// call into Renew.java
+			System.out.println("renew was called");
+		} else if (param.equals("all")) {
+			// call to do everything
+			System.out.println("all was called");
+		} else {
+			System.out.println("Invalid parameters!");
+			printUsage();
+			System.exit(1);
+		}
 	}
 }
